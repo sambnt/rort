5fa6c45c12dabbed2a9742fe6a5f7f88843a6ef1
Samuel Evans-Powell <mail@sevanspowell.net>
2024-02-05 15:00:43 +0800

    Group swapchain-dependent code

--- src/Rort/Examples/Triangle.hs
+++ src/Rort/Examples/Triangle.hs
@@ -1,16 +1,17 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds #-}
 
 module Rort.Examples.Triangle where
 
 import Rort.Window (withWindow, getRequiredExtensions, getWindowEvent, closeWindow)
 import Rort.Vulkan.Context (withVkContext, VkSettings (..), VkContext (..), graphicsQueueFamilies)
-import Control.Monad.Trans.Resource (runResourceT)
+import Control.Monad.Trans.Resource (runResourceT, MonadResource)
 import qualified Data.Vector as Vector
 import qualified Vulkan as Vk
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as BS
 import qualified Data.List.NonEmpty as NE
-import Rort.Render.Swapchain (withSwapchain, vkSurfaceFormat, vkImageViews, vkExtent, fsFenceInFlight, withNextFrameInFlight, fsSemaphoreImageAvailable, vkSwapchain, throwSwapchainOutOfDate, fsSemaphoreRenderFinished, throwSwapchainSubOptimal)
+import Rort.Render.Swapchain (withSwapchain, vkSurfaceFormat, vkImageViews, vkExtent, fsFenceInFlight, withNextFrameInFlight, fsSemaphoreImageAvailable, vkSwapchain, throwSwapchainOutOfDate, fsSemaphoreRenderFinished, throwSwapchainSubOptimal, Swapchain)
 import Rort.Vulkan (withVkShaderModule, withVkPipelineLayout, withVkRenderPass, withVkGraphicsPipelines, withVkFramebuffer, withVkCommandBuffers, withVkCommandPool)
 import qualified Vulkan.Extensions.VK_KHR_surface as VkFormat
 import qualified Vulkan.Zero as Vk
@@ -22,6 +23,8 @@ import Control.Monad (forM, forM_, when)
 import Data.Functor (void)
 import Foreign (nullPtr)
 import Rort.Window.Types (WindowEvent(..))
+import Data.Vector (Vector)
+import Rort.Util.Resource (Resource)
 
 main :: IO ()
 main = do
@@ -90,172 +93,12 @@ main = do
 
 
       -- BEGIN swapchain-dependent
-      renderPass
-        <- withVkRenderPass (vkDevice ctx)
-             $ Vk.RenderPassCreateInfo
-                 ()
-                 Vk.zero
-                 -- attachment descriptions
-                 ( Vector.singleton $ Vk.AttachmentDescription
-                     Vk.zero
-                     (VkFormat.format $ vkSurfaceFormat $ Resource.get swapchain)
-                     Vk.SAMPLE_COUNT_1_BIT            -- Samples
-                     Vk.ATTACHMENT_LOAD_OP_CLEAR      -- Load op, what to do with framebuffer before rendering
-                     Vk.ATTACHMENT_STORE_OP_STORE     -- Store op, store the framebuffer
-                     Vk.ATTACHMENT_LOAD_OP_DONT_CARE  -- Stencil load op
-                     Vk.ATTACHMENT_STORE_OP_DONT_CARE -- Stencil store op
-                     Vk.IMAGE_LAYOUT_UNDEFINED        -- Initial layout
-                     Vk.IMAGE_LAYOUT_PRESENT_SRC_KHR  -- Final layout
-                 )
-                 -- subpass descriptions
-                 ( Vector.singleton $ Vk.SubpassDescription
-                     Vk.zero
-                     Vk.PIPELINE_BIND_POINT_GRAPHICS
-                     Vector.empty -- input attachments
-                     -- color attachments
-                     ( Vector.singleton $ Vk.AttachmentReference
-                         0 -- attachment ix
-                         Vk.IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL
-                     )
-                     Vector.empty -- resolve attachments
-                     Nothing      -- depth stencil attachments
-                     Vector.empty -- preserve attachments
-                 )
-                 -- subpass dependencies
-                 ( Vector.singleton $ Vk.SubpassDependency
-                     Vk.SUBPASS_EXTERNAL -- src subpass
-                     0 -- dst subpass
-                     Vk.PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT -- src stage mask
-                     Vk.PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT -- dst stage mask
-                     Vk.zero -- src access mask
-                     Vk.ACCESS_COLOR_ATTACHMENT_WRITE_BIT -- dst access mask
-                     Vk.zero -- dependency flags
-                 )
-
-      pipelines <-
-        withVkGraphicsPipelines
-          (vkDevice ctx)
-          Vk.NULL_HANDLE -- pipeline cache
-          ( Vector.singleton
-            $ Vk.GraphicsPipelineCreateInfo () Vk.zero
-                (fromIntegral $ length pipelineShaderStages)
-                (Vk.SomeStruct <$> Vector.fromList pipelineShaderStages)
-                ( Just . Vk.SomeStruct
-                  $ Vk.PipelineVertexInputStateCreateInfo
-                      ()
-                      Vk.zero
-                      Vector.empty
-                      Vector.empty
-                )
-                ( Just
-                  $ Vk.PipelineInputAssemblyStateCreateInfo
-                      Vk.zero
-                      Vk.PRIMITIVE_TOPOLOGY_TRIANGLE_LIST -- Topology
-                      False -- Primitive restart
-                )
-                Nothing -- tesselation
-                ( Just . Vk.SomeStruct
-                  $ Vk.PipelineViewportStateCreateInfo
-                      ()
-                      Vk.zero
-                      -- Viewports (dynamic so empty)
-                      1
-                      Vector.empty
-                      -- Scissors (dynamic so empty)
-                      1
-                      Vector.empty
-                )
-                ( Just . Vk.SomeStruct
-                 $ Vk.PipelineRasterizationStateCreateInfo
-                     ()
-                     Vk.zero
-                     False -- Depth clamp
-                     False -- Rasterizer discard
-                     Vk.POLYGON_MODE_FILL -- Polygon mode
-                     Vk.CULL_MODE_NONE -- Cull mode
-                     Vk.FRONT_FACE_CLOCKWISE -- Facing
-                     False -- Depth bias
-                     0 -- Depth bias constant factor
-                     0 -- Depth bias clamp
-                     0 -- Depth bias slope factor
-                     1 -- Line width
-                )
-                ( Just . Vk.SomeStruct
-                    $ Vk.PipelineMultisampleStateCreateInfo
-                        ()
-                        Vk.zero
-                        Vk.SAMPLE_COUNT_1_BIT -- Number of samples
-                        False -- Sample shading enable
-                        1 -- Min sample shading
-                        Vector.empty -- Sample mask
-                        False -- Alpha to coverage
-                        False -- Alpha to one
-                )
-                ( Just
-                    $ Vk.PipelineDepthStencilStateCreateInfo
-                        Vk.zero
-                        True -- depth test enable
-                        True -- depth write enable
-                        Vk.COMPARE_OP_LESS -- compare op
-                        False -- bounds test
-                        False -- stencil test enable
-                        Vk.zero -- front stencil
-                        Vk.zero -- back stencil
-                        0 -- min depth
-                        1 -- max depth
-                )
-                ( Just . Vk.SomeStruct
-                    $ Vk.PipelineColorBlendStateCreateInfo
-                        ()
-                        Vk.zero
-                        False -- Logic op enable
-                        Vk.LOGIC_OP_COPY -- logic op
-                        1 -- attachment count
-                        -- attachments
-                        ( Vector.singleton
-                          $ Vk.PipelineColorBlendAttachmentState
-                              False -- blend enable
-                              Vk.zero -- src color blend factor
-                              Vk.zero -- dst color blend factor
-                              Vk.zero -- color blend op
-                              Vk.zero -- src alpha blend factor
-                              Vk.zero -- dst alpha blend factor
-                              Vk.zero -- alpha blend op
-                              -- color write mask
-                              (Vk.COLOR_COMPONENT_R_BIT .|. Vk.COLOR_COMPONENT_G_BIT .|. Vk.COLOR_COMPONENT_B_BIT .|. Vk.COLOR_COMPONENT_A_BIT)
-                        )
-                        (0, 0, 0, 0) -- Blend constants
-                )
-                ( Just
-                    $ Vk.PipelineDynamicStateCreateInfo
-                        Vk.zero
-                        (Vector.fromList [ Vk.DYNAMIC_STATE_VIEWPORT
-                                         , Vk.DYNAMIC_STATE_SCISSOR
-                                         ]
-                        )
-                )
-                (Resource.get pipelineLayout)
-                (Resource.get renderPass)
-                0 -- subpass index
-                Vk.NULL_HANDLE -- pipeline handle (inheritance)
-                (-1) -- pipeline index (inheritance)
-          )
-
-      let renderPasses = [(renderPass, pipelines)]
       framebuffers <-
-        forM (vkImageViews $ Resource.get swapchain) $ \iv -> do
-          forM renderPasses $ \(rp, ps) -> do
-            framebuffer <- withVkFramebuffer (vkDevice ctx)
-              $ Vk.FramebufferCreateInfo
-                  ()
-                  Vk.zero
-                  (Resource.get rp) -- render pass
-                  (Vector.singleton iv) -- attachments
-                  -- dimensions
-                  (Extent2D.width $ vkExtent $ Resource.get swapchain)
-                  (Extent2D.height $ vkExtent $ Resource.get swapchain)
-                  1 -- layers
-            pure (rp, ps, framebuffer)
+        perSwapchain
+          ctx
+          (Resource.get swapchain)
+          pipelineShaderStages
+          (Resource.get pipelineLayout)
       -- END swapchain-dependent
 
       cmdPool <-
@@ -398,3 +241,185 @@ main = do
             Nothing -> pure True
           when shouldContinue loop
       loop
+
+perSwapchain
+  :: MonadResource m
+  => VkContext
+  -> Swapchain
+  -> [Vk.PipelineShaderStageCreateInfo '[]]
+  -> Vk.PipelineLayout
+  -> m (Vector [( Resource Vk.RenderPass
+                , Resource (Vector Vk.Pipeline)
+                , Resource Vk.Framebuffer
+                )
+               ]
+       )
+perSwapchain ctx swapchain pipelineShaderStages pipelineLayout = do
+  renderPass
+    <- withVkRenderPass (vkDevice ctx)
+         $ Vk.RenderPassCreateInfo
+             ()
+             Vk.zero
+             -- attachment descriptions
+             ( Vector.singleton $ Vk.AttachmentDescription
+                 Vk.zero
+                 (VkFormat.format $ vkSurfaceFormat swapchain)
+                 Vk.SAMPLE_COUNT_1_BIT            -- Samples
+                 Vk.ATTACHMENT_LOAD_OP_CLEAR      -- Load op, what to do with framebuffer before rendering
+                 Vk.ATTACHMENT_STORE_OP_STORE     -- Store op, store the framebuffer
+                 Vk.ATTACHMENT_LOAD_OP_DONT_CARE  -- Stencil load op
+                 Vk.ATTACHMENT_STORE_OP_DONT_CARE -- Stencil store op
+                 Vk.IMAGE_LAYOUT_UNDEFINED        -- Initial layout
+                 Vk.IMAGE_LAYOUT_PRESENT_SRC_KHR  -- Final layout
+             )
+             -- subpass descriptions
+             ( Vector.singleton $ Vk.SubpassDescription
+                 Vk.zero
+                 Vk.PIPELINE_BIND_POINT_GRAPHICS
+                 Vector.empty -- input attachments
+                 -- color attachments
+                 ( Vector.singleton $ Vk.AttachmentReference
+                     0 -- attachment ix
+                     Vk.IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL
+                 )
+                 Vector.empty -- resolve attachments
+                 Nothing      -- depth stencil attachments
+                 Vector.empty -- preserve attachments
+             )
+             -- subpass dependencies
+             ( Vector.singleton $ Vk.SubpassDependency
+                 Vk.SUBPASS_EXTERNAL -- src subpass
+                 0 -- dst subpass
+                 Vk.PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT -- src stage mask
+                 Vk.PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT -- dst stage mask
+                 Vk.zero -- src access mask
+                 Vk.ACCESS_COLOR_ATTACHMENT_WRITE_BIT -- dst access mask
+                 Vk.zero -- dependency flags
+             )
+
+  pipelines <-
+    withVkGraphicsPipelines
+      (vkDevice ctx)
+      Vk.NULL_HANDLE -- pipeline cache
+      ( Vector.singleton
+        $ Vk.GraphicsPipelineCreateInfo () Vk.zero
+            (fromIntegral $ length pipelineShaderStages)
+            (Vk.SomeStruct <$> Vector.fromList pipelineShaderStages)
+            ( Just . Vk.SomeStruct
+              $ Vk.PipelineVertexInputStateCreateInfo
+                  ()
+                  Vk.zero
+                  Vector.empty
+                  Vector.empty
+            )
+            ( Just
+              $ Vk.PipelineInputAssemblyStateCreateInfo
+                  Vk.zero
+                  Vk.PRIMITIVE_TOPOLOGY_TRIANGLE_LIST -- Topology
+                  False -- Primitive restart
+            )
+            Nothing -- tesselation
+            ( Just . Vk.SomeStruct
+              $ Vk.PipelineViewportStateCreateInfo
+                  ()
+                  Vk.zero
+                  -- Viewports (dynamic so empty)
+                  1
+                  Vector.empty
+                  -- Scissors (dynamic so empty)
+                  1
+                  Vector.empty
+            )
+            ( Just . Vk.SomeStruct
+             $ Vk.PipelineRasterizationStateCreateInfo
+                 ()
+                 Vk.zero
+                 False -- Depth clamp
+                 False -- Rasterizer discard
+                 Vk.POLYGON_MODE_FILL -- Polygon mode
+                 Vk.CULL_MODE_NONE -- Cull mode
+                 Vk.FRONT_FACE_CLOCKWISE -- Facing
+                 False -- Depth bias
+                 0 -- Depth bias constant factor
+                 0 -- Depth bias clamp
+                 0 -- Depth bias slope factor
+                 1 -- Line width
+            )
+            ( Just . Vk.SomeStruct
+                $ Vk.PipelineMultisampleStateCreateInfo
+                    ()
+                    Vk.zero
+                    Vk.SAMPLE_COUNT_1_BIT -- Number of samples
+                    False -- Sample shading enable
+                    1 -- Min sample shading
+                    Vector.empty -- Sample mask
+                    False -- Alpha to coverage
+                    False -- Alpha to one
+            )
+            ( Just
+                $ Vk.PipelineDepthStencilStateCreateInfo
+                    Vk.zero
+                    True -- depth test enable
+                    True -- depth write enable
+                    Vk.COMPARE_OP_LESS -- compare op
+                    False -- bounds test
+                    False -- stencil test enable
+                    Vk.zero -- front stencil
+                    Vk.zero -- back stencil
+                    0 -- min depth
+                    1 -- max depth
+            )
+            ( Just . Vk.SomeStruct
+                $ Vk.PipelineColorBlendStateCreateInfo
+                    ()
+                    Vk.zero
+                    False -- Logic op enable
+                    Vk.LOGIC_OP_COPY -- logic op
+                    1 -- attachment count
+                    -- attachments
+                    ( Vector.singleton
+                      $ Vk.PipelineColorBlendAttachmentState
+                          False -- blend enable
+                          Vk.zero -- src color blend factor
+                          Vk.zero -- dst color blend factor
+                          Vk.zero -- color blend op
+                          Vk.zero -- src alpha blend factor
+                          Vk.zero -- dst alpha blend factor
+                          Vk.zero -- alpha blend op
+                          -- color write mask
+                          (Vk.COLOR_COMPONENT_R_BIT .|. Vk.COLOR_COMPONENT_G_BIT .|. Vk.COLOR_COMPONENT_B_BIT .|. Vk.COLOR_COMPONENT_A_BIT)
+                    )
+                    (0, 0, 0, 0) -- Blend constants
+            )
+            ( Just
+                $ Vk.PipelineDynamicStateCreateInfo
+                    Vk.zero
+                    (Vector.fromList [ Vk.DYNAMIC_STATE_VIEWPORT
+                                     , Vk.DYNAMIC_STATE_SCISSOR
+                                     ]
+                    )
+            )
+            pipelineLayout
+            (Resource.get renderPass)
+            0 -- subpass index
+            Vk.NULL_HANDLE -- pipeline handle (inheritance)
+            (-1) -- pipeline index (inheritance)
+      )
+
+  let renderPasses = [(renderPass, pipelines)]
+  framebuffers <-
+    forM (vkImageViews swapchain) $ \iv -> do
+      forM renderPasses $ \(rp, ps) -> do
+        framebuffer <- withVkFramebuffer (vkDevice ctx)
+          $ Vk.FramebufferCreateInfo
+              ()
+              Vk.zero
+              (Resource.get rp) -- render pass
+              (Vector.singleton iv) -- attachments
+              -- dimensions
+              (Extent2D.width $ vkExtent swapchain)
+              (Extent2D.height $ vkExtent swapchain)
+              1 -- layers
+        pure (rp, ps, framebuffer)
+
+  pure framebuffers
